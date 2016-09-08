f8cec603e5d22e41835eb2dc36a58c4b9733b402
Egar Rizki <egarg4ul@gmail.com>
2016-09-08 23:46:04 +0700

    first commit
    first commit

---
+++ index.php
@@ -0,0 +1,148 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<title>Simple XKCD Password Generator</title>
+
+		<meta charset="UTF-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
+		<link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Oswald:700,400%7CDroid+Serif:400,400italic,700" />
+		<link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" />
+
+		<style type="text/css">
+			footer {
+				position: absolute;
+				bottom: 0;
+				color: #fff;
+				width: 100%;
+				background-color: #222;
+				padding: 10px;
+				height: 30px;
+			}
+		</style>
+	</head>
+
+	<body style="background-color:#eee;">
+		<?php require 'xkcd.php' ?>
+
+		<div class="container">
+			<div class="row">
+				<div class="col-md-12 text-center">
+					<h1><span class="label label-info">XKCD Password Generator</span></h1>
+				</div>
+			</div>
+
+			<br/>
+
+			<?php
+				if(!empty($password_string)):
+			?>
+			<div class="row">
+				<div class="col-sm-12 text-center">
+					<span class="label label-success"><?php echo $password_string; ?></span>
+				</div>
+			</div>
+
+			<br />
+			<?php
+				endif;
+			?>
+
+			<div class="row">
+				<div class="col-md-3"></div>
+
+				<div class="col-md-6">
+					<ul class="nav nav-tabs" role="tablist">
+						<li role="presentation" class="active">
+							<a href="#generate" aria-controls="generate" role="tab" data-toggle="tab">Generate</a>
+						</li>
+
+						<li role="presentation">
+							<a href="#info" aria-controls="info" role="tab" data-toggle="tab">Info</a>
+						</li>
+					</ul>
+
+					<div class="tab-content">
+						<div role="tabpanel" class="tab-pane active" id="generate">
+							<form id="newpassword" action="index.php" method="post" class="form-horizontal">
+								<table class="table table-hover table-striped table-responsive">
+									<tbody class="text-center">
+										<tr>
+											<td>Number of Words</td>
+
+											<td>
+												<input type="number" name="num" id="num" min="1" max="9" value="<?php echo $num; ?>" required class="form-control" />
+											</td>
+										</tr>
+
+										<tr>
+											<td>Include Number</td>
+
+											<td>
+												<select class="form-control" name="num_select">
+													<option>Select</option>
+													<option value="1">Yes, Include it.</option>
+													<option value="0">No, thanks.</option>
+												</select>
+											</td>
+										</tr>
+
+										<tr>
+											<td>Include Symbol</td>
+
+											<td>
+												<select class="form-control" name="symbol_select">
+													<option>Select</option>
+													<option value="1">Yes, Include it.</option>
+													<option value="0">No, thanks.</option>
+												</select>
+											</td>
+										</tr>
+
+										<tr>
+											<td>Separator</td>
+
+											<td>
+												<select class="form-control" name="separator">
+													<option>Select</option>
+													<option value="1">Space</option>
+													<option value="2">-</option>
+													<option value="3">.</option>
+													<option value="4">None</option>
+													<option value="5">Random</option>
+												</select>
+											</td>
+										</tr>
+
+										<tr>
+											<td colspan="2">
+												<button type="submit" name="submit" class="btn btn-success btn-lg btn-block">Generate</button>
+											</td>
+										</tr>
+									</tbody>
+								</table>
+							</form>
+						</div>
+
+						<div role="tabpanel" class="tab-pane" id="info">
+							<a href="http://xkcd.com/936/">
+								<img src="img/xkcd_password_strength.png" class="img-thumbnail" alt="XKCD Password Strength" />
+							</a>
+						</div>
+					</div>
+				</div>
+
+				<div class="col-md-3"></div>
+			</div>
+		</div>
+
+		<footer>
+			<p class="text-center" style="margin-top:-5px;">
+				XKCD Password Generator - Powered by <a href="https://getbootstrap.com" target="_blank">Bootstrap</a> &amp; <a href="https://jquery.com" target="_blank">jQuery</a>
+			</p>
+		</footer>
+
+		<script type="text/javascript" src="http://code.jquery.com/jquery-3.1.0.min.js"></script>
+		<script type="text/javascript" src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+	</body>
+</html>
---
+++ xkcd.php
@@ -0,0 +1,105 @@
+<?php
+	function reader($file, $key) {
+		$rawdata = file_get_contents($file);
+		$object = json_decode($rawdata, true);
+
+		return $object[$key];
+	}
+
+	$allverbs = reader("json/word.json", "word");
+	$verbs = [];
+
+	foreach($allverbs as $verb) {
+		array_push($verbs, $verb["past"]);
+	}
+
+	$symbols = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "="];
+	$verbs_length = count($verbs) - 1;
+	$symbols_length = count($symbols) - 1;
+	$num_on = "";
+	$num_off = "";
+	$symbol_on = "";
+	$symbol_off = "";
+	$separator_space = "";
+	$separator_dash = "";
+	$separator_dot = "";
+	$separator_none = "";
+	$separator_random = "";
+	$separators = [" ", "-", ".", ""];
+	$length = "";
+	$num = "";
+	$symbol = "";
+
+	if(isset($_POST["submit"])) {
+		if(isset($_POST["num"])) {
+			$length = $_POST["num"] <= 15 ? $_POST["num"]:0;
+		}
+
+		$num = $_POST["num_select"];
+
+		if($num == "1") {
+			$num_on = "1";
+		} elseif($num == "0") {
+			$num_off = "0";
+		}
+
+		$symbol = $_POST["symbol_select"];
+
+		if($symbol == "1") {
+			$symbol_on = "1";
+		} elseif($symbol == "0") {
+			$symbol_off = "0";
+		}
+
+		$separator = $_POST["separator"];
+
+		if(!empty($separator)) {
+			switch($separator) {
+				case 1:
+					$separator_space = " ";
+					$separator = $separators[0];
+				break;
+
+				case 2:
+					$separator_dash = "-";
+					$separator = $separators[1];
+				break;
+
+				case 3:
+					$separator_space = ".";
+					$separator = $separators[2];
+				break;
+
+				case 4:
+					$separator_none = "";
+					$separator = $separators[3];
+				break;
+
+				case 5:
+					$separator_random = "random";
+					$separator = $separators[rand(0, (count($separators) - 1))];
+				break;
+
+				default:
+					$separator_random = "random";
+					$separator = $separators[rand(0, (count($separators) - 1))];
+				break;
+			}
+		}
+
+		$password_string = "";
+
+		for($i = 0; $i < $length; $i++) {
+			if($num == "1" && $symbol == "1") {
+				$password_string = $password_string . str_shuffle($verbs[rand(0, $verbs_length)] . rand(0, 9) . $symbols[rand(0, $symbols_length)]) . $separator;
+			} elseif($num == "1" && $symbol == "0") {
+				$password_string = $password_string . str_shuffle($verbs[rand(0, $verbs_length)] . rand(0,9)) . $separator;
+			} elseif($symbol == "1" && $num == "0") {
+				$password_string = $password_string . str_shuffle($verbs[rand(0, $verbs_length)] . $symbols[rand(0, $symbols_length)]) . $separator;
+			} else {
+				$password_string = $password_string . $verbs[rand(0, $verbs_length)] . $separator;
+			}
+
+			$password_string = strtolower($password_string);
+		}
+	}
